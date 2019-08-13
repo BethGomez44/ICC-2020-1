58bb0912b646721b6a9e81a62e648f019565596b
BethGomez44 <liz_gomez04@ciencias.unam.mx>
2019-08-13 11:32:30 -0500

    Nuevos archivos

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[]args){
+	System.out.println("Hola gg");
+	System.out.println("openjdk version 11.0.2  2019-01-15");
+	System.out.println("OpenJDK Runtime Environment 18.9 (build 11.0.2+9)");
+        System.out.println("OpenJDK 64-Bit Server VM 18.9 (build 11.0.2+9, mixed mode)");	
+    }
+}
