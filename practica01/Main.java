7425f629a98da0822028c08c21595e6046f84e1a
BethGomez44 <53274554+BethGomez44@users.noreply.github.com>
2019-08-16 22:07:08 -0500

    Nuevo

--- practica01/Main.java
+++ practica01/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[]args){
 	      System.out.println("openjdk version 11.0.2  2019-01-15");
 	      System.out.println("OpenJDK Runtime Environment 18.9 (build 11.0.2+9)");
-        System.out.println("OpenJDK 64-Bit Server VM 18.9 (build 11.0.2+9, mixed mode)");
+              System.out.println("OpenJDK 64-Bit Server VM 18.9 (build 11.0.2+9, mixed mode)");
+	    
+	    System.out.println("Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux)");
     }
 }
