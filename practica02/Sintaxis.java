fd66019f1cf6a373d4997f4465931ed4a03a79f4
BethGomez44 <liz_gomez04@ciencias.unam.mx>
2019-08-17 21:52:17 -0500

    Nuevos

---
+++ practica02/Sintaxis.java
@@ -0,0 +1,25 @@
+import static java.lang.Math.sqrt;
+
+public class Sintaxis {
+
+	public static void main(String[] args) {
+		int a, b = 20, c;
+		a = 3; c = -7;
+
+		boolean prop = true;
+		byte ochoBits = 120;
+		short dieciseisBits = -32760;
+
+		double discriminante = b * b - 4 * a * c;
+		double x1 = (-b + sqrt(discriminante)) / (2 * a);
+		double x2 = (-b - sqrt(discriminante)) / (2 * a);
+
+		char ch1, ch2;
+		ch1 = 'g'; ch2 = 'w';
+		int d = 1 / ((a - 3)+1);
+
+		// Declaraciones
+
+	}
+
+}
