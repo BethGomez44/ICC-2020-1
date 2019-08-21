3586143bb4f6ea9d960d4dbc626fb80807a50e4a
BethGomez44 <53274554+BethGomez44@users.noreply.github.com>
2019-08-20 22:21:28 -0500

    Add files via upload

---
+++ practica03/Ecuacion.java
@@ -0,0 +1,17 @@
+public class Ecuacion {
+
+	public static void main(String[] args) {
+		double a = 3, b = 17, c = 10, d = -5.7, e = 3.5;
+		int i = 1, n = 10*10*10*10*10*10*10*10;
+		double suma = 0;
+		while (i <= n) {
+			/* Escribe en la línea 9 */
+			suma+= -550*(a+(i*(b-a))/n)*(a+(i*(b-a))/n)+2992*(a+(i*(b-a))/n);
+			suma-= ((49*c)*(d+e))/(34*(3*d+c));
+			/* ************************ */
+			i++;
+		}
+		System.out.println("Resultado = " + (((b - a) / n) * suma));
+	}
+
+}
