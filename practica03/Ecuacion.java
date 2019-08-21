8336d84f235c17dfc9af532c78c28d1ff686a823
BethGomez44 <53274554+BethGomez44@users.noreply.github.com>
2019-08-20 22:22:15 -0500

    Update Ecuacion.java

--- practica03/Ecuacion.java
+++ practica03/Ecuacion.java
@@ -2,7 +2,7 @@ public class Ecuacion {
 
 	public static void main(String[] args) {
 		double a = 3, b = 17, c = 10, d = -5.7, e = 3.5;
-		int i = 1, n = 10*10*10*10*10*10*10*10;
+		int i = 1, n = 10;
 		double suma = 0;
 		while (i <= n) {
 			/* Escribe en la línea 9 */
